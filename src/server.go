dbabf5b3980ffba6760073cebe72e2f479c8a1a6
skisa31 <skis1031@gmail.com>
2019-12-10 23:17:30 +0900

    エラー404と500を追加した

--- src/server.go
+++ src/server.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"net/http"
-	"github.com/labstack/echo"
 	"html/template"
 	"io"
+	"errors"
+
+	"github.com/labstack/echo"
 )
 
 // テンプレートのレンダラーを作る
 type Renderer struct {
 	templates *template.Template
 }
+
+var NotFoundError = errors.New("NotFound")
 
 func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return r.templates.ExecuteTemplate(w, name, data)
@@ -24,9 +28,23 @@ func main() {
 		templates: template.Must(template.ParseGlob("plan2/*.html")),
 	}
 
+	e.HTTPErrorHandler = func(err error, c echo.Context) {
+		if he, ok := err.(*echo.HTTPError); ok {
+			if he.Code == 404 {
+				c.Render(http.StatusNotFound,"404",nil)
+			} else {
+				c.Render(http.StatusInternalServerError,"500",nil)
+			}
+		}
+	}
+
 	e.GET("/trend", func(c echo.Context) error {
 		// plan2/trend_page.htmlがサーブされる
 		return c.Render(http.StatusOK, "trend_page", nil)
+	})
+
+	e.GET("/cart", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "ContentInTheCart", nil)
 	})
 
 	// 静的ファイルのため
